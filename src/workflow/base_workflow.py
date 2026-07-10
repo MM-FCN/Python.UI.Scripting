bfaaa1f05e43260ef7384d002c472ed46c10cec7
Oscar Gao <gas8szh@bosch.com>
2026-07-10 14:56:47 +0800

    remove unused args

--- src/workflow/base_workflow.py
+++ src/workflow/base_workflow.py
@@ -665,9 +665,6 @@ class WorkflowCrawler:
                     edge_extra_args = []
                 if self.headless:
                     options.add_argument("--headless=new")
-                    options.add_argument("--window-size=1920,1080")
-                    options.add_argument("--disable-gpu")
-                    options.add_argument("--no-sandbox")
                     options.add_argument("--force-device-scale-factor=1")
                     options.add_argument("--disable-blink-features=AutomationControlled")
                 else:
